72f14ac0f75fe6146059235b521f578b314317ce
Kenil Vora <kenilvora2509@gmail.com>
2024-09-06 18:10:28 +0530

    Bug Fixes

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://learning-studynotion.vercel.app/",
+    origin: "https://learning-studynotion.vercel.app",
     credentials: true,
   })
 );
