22cc5b6888901bac9f7b680b91753a76c5c1212a
Kenil Vora <kenilvora2509@gmail.com>
2024-11-25 17:57:30 +0530

    Bug Fixes

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -205,7 +205,8 @@ exports.login = async (req, res) => {
           // create cookie and send response
           res.cookie("token", token, {
             secure: true,
-            sameSite: "lax",
+            httpOnly: true,
+            sameSite: "None",
           });
 
           res.status(200).json({
